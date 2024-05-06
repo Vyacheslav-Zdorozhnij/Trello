7aa6ffc6b64e174ca1288fdfc62d3c4200f7e62d
slavik6661 <slavik0.75@mail.ru>
2024-05-06 09:39:32 +0300

    add menu_board

---
+++ trello/src/components/header2/header-memu.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Image from "next/image";
+import style from "./heder.module.css";
+export function Header2Component() {
+  return (
+    <div className={style.header}>
+      <p>Brackets</p>
+      <div className={style.iconContainer}>
+        <Image
+          src="/header2-icons/Hard.svg"
+          width={19}
+          height={19}
+          alt={"hard"}
+        ></Image>
+
+        <Image
+          src="/header2-icons/Heart.svg"
+          width={19}
+          height={19}
+          alt={"hard"}
+        ></Image>
+
+        <Image
+          src="/header2-icons/Public.svg"
+          width={88}
+          height={35}
+          alt={"hard"}
+        ></Image>
+      </div>
+      <div className={style.userMenu}>
+        <Image
+          src="/header2-icons/Users.svg"
+          width={140}
+          height={40}
+          alt={"hard"}
+        ></Image>
+        <button>Menu</button>
+      </div>
+    </div>
+  );
+}
